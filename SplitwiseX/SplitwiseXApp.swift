1162590a1313d67e6fa73a8c6e4c3627baf2308b
Johnny Jurak <johny9020@hotmail.com>
2023-12-26 20:04:33 +0100

    Initial Commit

---
+++ SplitwiseX/SplitwiseXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitwiseXApp.swift
+//  SplitwiseX
+//
+//  Created by Johnny Jurak on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SplitwiseXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
